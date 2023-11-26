d88f4796c0b8cbd61f018cf9a9cc35a7ea2ff237
정소민 <yjk70702002@gmail.com>
2023-11-27 00:00:08 +0900

    initial commit

---
+++ src/main/java/uxdesign/cafemap/CafemapApplication.java
@@ -0,0 +1,14 @@
+package uxdesign.cafemap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CafemapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CafemapApplication.class, args);
+		System.out.println("cafemap");
+	}
+
+}
---
+++ src/test/java/uxdesign/cafemap/CafemapApplicationTests.java
@@ -0,0 +1,13 @@
+package uxdesign.cafemap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CafemapApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
